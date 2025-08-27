a5aeb8bb7391efcdd9328ac6ab455e92419deecb
heather4842 <162910117+crazyapoll0@users.noreply.github.com>
2025-08-27 17:06:16 +0900

    리엑트 끝

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import Library from './ch03/Library'
 import Button from './ch04/Button';
 import ConfirmDialog from './ch04/Button';
@@ -9,10 +9,12 @@ import Clock from './ch04/Clock';
 import CommentList from './ch05/CommemtList';
 import AttdanceBook from './ch10/AttendanceBook';
 import SignUp from './ch11/SignUp';
-
+import Calculator from './ch12/Calculator';
+import ProfileCard from './ch13/ProfileCard';
+import App from './ch14/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <SignUp />
+  <App />
 );
 
 
